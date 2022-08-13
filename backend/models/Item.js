325ddc2d16c3e1c4e88eb2a5f74cf7ce6f0df13a
jon-vdb <jonvdberg8@gmail.com>
2022-08-14 02:06:37 +0700

    db fix

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -9,7 +9,7 @@ var ItemSchema = new mongoose.Schema(
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: String || placeholder,
+    image: String,
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
